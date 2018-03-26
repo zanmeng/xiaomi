06412404b5377770033732536ab530fdd1cbb456
shiyuanqiang <1597106647@qq.com>
2018-03-26 19:44:33 +0800

    我的第一次

---
+++ app/Http/Controllers/admin/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index(){
+        return view('admin.index');
+    }
+
+    public  function info(){
+        return view('admin.welcome');
+    }
+
+    //退出登录
+    public function logout()
+    {
+        session()->forget('user');
+        return redirect('admin/login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,48 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin/index');
+});*/
+
+
+
+Route::group(['prefix'=>'admin','namespace'=>'admin'],function(){
+
+    //后台登录
+    Route::get('login','LoginController@login');
+    //生成验证码
+    Route::get('code','LoginController@code');
+    // 第三方组件生成验证码的路由
+    Route::get('code/captcha/{userId}','LoginController@captcha');
+
+    //登录处理路由
+    Route::post('dologin','LoginController@dologin');
+
+    Route::get('jiami','LoginController@jiami');
 });
+
+
+    //后台
+Route::group(['prefix'=>'admin','namespace'=>'admin','middleware'=>'isLogin'],function(){
+
+    //后天首页
+    Route::get('index','IndexController@index');
+
+    //后天信息页
+    Route::get('info','IndexController@info');
+
+    //退出  登录
+    Route::get('logout','IndexController@logout');
+
+    //后台用户模块
+    //启用  禁用
+    Route::post('user/changestatus','UserController@changestatus');
+
+    //批量删除
+    Route::get('user/delall','UserController@delall');
+
+    Route::resource('user','UserController');
+});
+
