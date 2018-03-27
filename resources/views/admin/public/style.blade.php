15848f9a4027a21faeabfb2e010c30015dd80611
lll <240477476@qq.com>
2018-03-27 10:19:32 +0800

    刘路路.前台登录注册

--- resources/views/admin/public/style.blade.php
+++
@@ -1,3 +0,0 @@
-
-<link rel="stylesheet" href="{{asset('admin/css/font.css')}}">
-<link rel="stylesheet" href="{{asset('admin/css/xadmin.css')}}">
--- routes/web.php
+++ routes/web.php
@@ -15,42 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix'=>'admin','namespace'=>'admin'],function(){
-
-    //后台登录
-    Route::get('login','LoginController@login');
-//生成验证码
-    Route::get('code','LoginController@code');
-// 第三方组件生成验证码的路由
-    Route::get('code/captcha/{id}','LoginController@captcha');
-
-//登录处理路由
-    Route::post('dologin','LoginController@dologin');
-
-    Route::get('jiami','LoginController@jiami');
-});
-
-Route::group(['prefix'=>'admin','namespace'=>'admin','middleware'=>'isLogin'],function() {
-    //后台主页
-    Route::get('index', 'IndexController@index');
-    //后台欢迎页
-    Route::get('welcome', 'IndexController@welcome');
-    //退出登录
-    Route::get('logout', 'IndexController@logout');
-
-    Route::resource('user','UserController');
-
-});
-
-
-
-
-
-
-
-
-
-
 //前台
 //前台首页
 Route::get('home/index','home\IndexController@index');
