24713a9ad14df21c22f2261eafc80fd3b1e3c079
zanmeng <1916150942@qq.com>
2018-03-27 22:50:48 +0800

    cheng?

--- app/Http/Controllers/admin/LoginController.php
+++ app/Http/Controllers/admin/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
 use App\Model\User;
 use App\Org\code\Code;
--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
 use App\Model\User;
 use function foo\func;
